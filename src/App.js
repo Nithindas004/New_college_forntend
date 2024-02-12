5c49e3a729faca61fb0bafb19f4d8f363a23c605
Nithindas004 <nithindas232001@gmail.com>
2024-02-12 16:28:39 +0530

    student login completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AdminLogin from './components/AdminLogin';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import StudentLogin from './components/StudentLogin';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Routes>
+        <Route path='/' element={<StudentLogin/>} />
         <Route path='/admin' element={<AdminLogin/>} />
       </Routes>
       </BrowserRouter>
